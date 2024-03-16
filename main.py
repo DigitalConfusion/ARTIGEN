fcb9145f27662d314265b2d34d858637fbd3fbb6
FoxRio <davis.krasts@gmail.com>
2024-03-16 12:23:36 +0200

    feat: example callout

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from dotenv import load_dotenv
+from openai import OpenAI
 import os
 load_dotenv()
 
-api = os.getenv('API_KEY')
+api = os.getenv('OPENAI_API_KEY')
 print(api)
+from openai import OpenAI
+client = OpenAI(
+  api_key=api
+)
+
+response = client.images.generate(
+  model="dall-e-2",
+  prompt="line art of a white siamese cat",
+  size="256x256",
+  quality="standard",
+  n=1,
+)
+
+image_url = response.data[0].url
