a963999ff06616e4e982ab084768fc2bb8c089ab
FoxRio <davis.krasts@gmail.com>
2024-03-16 14:45:01 +0200

    feat: chatgpt.py

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import os
 load_dotenv()
 
 api = os.getenv('OPENAI_API_KEY')
-print(api)
+
 from openai import OpenAI
 client = OpenAI(
   api_key=api
 )
-
+generatedPromt = "98-year-old Meals on Wheels volunteer delivers meals to those in need"
 response = client.images.generate(
   model="dall-e-2",
-  prompt="line art of a white siamese cat",
+  prompt=generatedPromt,
   size="256x256",
   quality="standard",
   n=1,
 )
 
 image_url = response.data[0].url
+print(image_url)
